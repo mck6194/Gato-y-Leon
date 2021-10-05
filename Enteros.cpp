41052fae1497677222e2363713d0aa9c557c2860
mck6194 <91956222+mck6194@users.noreply.github.com>
2021-10-06 01:13:28 +0200

    Add files via upload

---
+++ Enteros.cpp
@@ -0,0 +1,7 @@
+#include <iostream>using namespace std; 
+	int main () {	int no1, no2, sum; 	cout << "\nEnter the first number = " ; 
+	cin >> no1 ;	cout << "\nEnter the second number = " ;
+	cin >> no2 ;
+ 	sum = no1 + no2 ;
+	cout << "\nThe sum of "<< no1 <<" and "<< no2 <<" = "<< sum <<'\n' ;	
+	return 0 ;}
