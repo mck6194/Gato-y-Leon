27ccc38d4dbfba273e82896fe3e584bdcbbf8e75
mck6194 <91956222+mck6194@users.noreply.github.com>
2021-11-19 22:04:35 +0100

    Add files via upload

---
+++ Buffer overflow/programa_overflow.c
@@ -0,0 +1,10 @@
+/* Ignacio Nieto Vidaurrazaga - inietv00@estudiantes.unileon.es */
+
+
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char** argv){
+	char buffer[500];
+	strcpy(buffer,argv[1]);
+	return 0;
+}
